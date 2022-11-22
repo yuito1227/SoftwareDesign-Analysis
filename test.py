c10f7c4d744821c3b6006bd7047bdf6518344e7c
yuito1227 <yuito3226@gmail.com>
2022-11-22 15:40:56 +0900

    lesson2

---
+++ test.py
@@ -0,0 +1,23 @@
+import unittest
+from CSVPrinter import CSVPrinter
+
+class Test_CSVPrinter(unittest.TestCase):
+    def test_LoadCurrentRows(self):
+        printer = CSVPrinter("sample.csv")
+        l = printer.read()
+        self.assertEqual(3,len(l))
+
+    def test_LoadCurrentValues(self):
+        printer = CSVPrinter("sample.csv")
+        current_values = [
+            ["Value1A","Value1B","Value1C"],
+            ["Value2A","Value2B","Value2C"],
+            ["Value3A","Value3B","Valu›e3C"]
+            ]
+        l = printer.read()
+        self.assertEqual(current_values,l)
+
+    def test_FileNotFound(self):
+        with self.assertRaises(FileNotFoundError):
+            printer = CSVPrinter("sampl.csv")
+            l = printer.read()
---
+++ test1.py
@@ -0,0 +1,5 @@
+import unittest
+def setUpModule():
+    print("Running setUpModule")
+def tearDownModule():
+    print("Running tearDownModule")
